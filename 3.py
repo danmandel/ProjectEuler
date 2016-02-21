36278e591ede668fb6edb926d76d84349cabd93d
danmandel <danmandel@users.noreply.github.com>
2016-02-20 19:44:19 -0500

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,11 +1,19 @@
 # Finds the largest prime factor of n
-# Code from stackoverflow
+# Main code from stackoverflow
 # Does not work for squares
 
+import time
+ 
+def find_largest_prime_factor(n):
+    i = 2
+    while i * i < n: # Sieve of Eratosthenes
+        while n % i == 0:
+            n = n // i
+        i += 1
+    return n
+
+start_time = time.clock()
 n = 600851475143
-i = 2
-while i * i < n:
-    while n % i == 0:
-        n = n // i
-    i += 1
-print (n)
+
+print (find_largest_prime_factor(n))
+print (time.clock() - start_time)
